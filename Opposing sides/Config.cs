ba5f7335c6c0a900cd863e6c1b89369beb6e0ba5
EgoriesGames <117574606+EgoriesGames@users.noreply.github.com>
2023-08-20 19:40:16 +0500

    Closed Update 0.1.1
    An improved version of the Tesla gate functionality.
    
    Uploading the repository to github.

---
+++ Opposing sides/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+
+namespace Opposing_sides
+{
+    public class Config : IConfig
+    {
+        [Description("Enable or disable the plugin.")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Not used")]
+        public bool Debug { get; set; }
+        [Description("If Reativate = true, then the Tesla gates will turn off if more Class D personnel escaped than scientists and will turn on if more scientists escaped than Class D personnel.")]
+        public bool Reactivate { get; set; } = true;
+        [Description("If Reactivate = false, then the Tesla gateы will be disabled after the number of Class D personnel specified in the EscapeCount parameter escapes.")]
+        public int EscapeCount { get; set; } = 3;
+    }
+}
---
+++ Opposing sides/Plugin.cs
@@ -0,0 +1,60 @@
+﻿#define BETA
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opposing_sides
+{
+    public class Plugin : Plugin<Config>
+    {
+        private Config config = new Config();
+        public override string Name => "Opposing sides";
+        public override string Author => "Sakred_";
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            #if BETA
+            Log.Warn("The plugin is in beta. More features will appear in the plugin soon. If bugs are found, leave a feedback by clicking on the link capybara-scpsl.com/feedback .");
+            #endif
+            Exiled.Events.Handlers.Player.TriggeringTesla += OnActivatingTesla;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.TriggeringTesla -= OnActivatingTesla;
+            base.OnDisabled();
+        }
+
+        public void OnActivatingTesla(TriggeringTeslaEventArgs ev)
+        {
+            if (config.Reactivate == true)
+            {
+                if (Round.EscapedDClasses > Round.EscapedScientists)
+                {
+                    ev.IsAllowed = false;
+                }
+                if (Round.EscapedScientists >= Round.EscapedDClasses)
+                {
+                    ev.IsAllowed = true;
+                }
+            }
+
+            if (config.Reactivate == false)
+            {
+                if (Round.EscapedDClasses >= config.EscapeCount)
+                {
+                    ev.IsAllowed = false;
+                }
+            }
+        }
+    }
+
+}
